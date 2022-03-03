5d52b880313e34413e630a228dfc96e85d1070ab
jeeljadawala <64966081+jeeljadawala@users.noreply.github.com>
2022-03-03 14:49:15 +0530

    added contact.js , about.js

--- src/pages/Navbar.js
+++ src/pages/Navbar.js
@@ -4,23 +4,23 @@ export const Navbar = () => {
   return (
       <>
      {/* <!-- Favicons -->*/}
-  <link href="assets/img/favicon.png" rel="icon"/>
-  <link href="assets/img/apple-touch-icon.png" rel="apple-touch-icon"/>
+  <link href="src/pages/assets/img/favicon.png" rel="icon"/>
+  <link href="src/pages/assets/img/apple-touch-icon.png" rel="apple-touch-icon"/>
 
   {/*<!-- Google Fonts -->*/}
   <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i|Raleway:300,300i,400,400i,500,500i,600,600i,700,700i|Poppins:300,300i,400,400i,500,500i,600,600i,700,700i" rel="stylesheet"/>
 
 
-  <link href="assets/vendor/aos/aos.css" rel="stylesheet"/>
-  <link href="assets/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet"/>
-  <link href="assets/vendor/bootstrap-icons/bootstrap-icons.css" rel="stylesheet"/>
-  <link href="assets/vendor/boxicons/css/boxicons.min.css" rel="stylesheet"/>
-  <link href="assets/vendor/glightbox/css/glightbox.min.css" rel="stylesheet"/>
-  <link href="assets/vendor/remixicon/remixicon.css" rel="stylesheet"/>
-  <link href="assets/vendor/swiper/swiper-bundle.min.css" rel="stylesheet"/>
+  <link href="src/pages/assets/vendor/aos/aos.css" rel="stylesheet"/>
+  <link href="src/pages/assets/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet"/>
+  <link href="src/pages/assets/vendor/bootstrap-icons/bootstrap-icons.css" rel="stylesheet"/>
+  <link href="src/pages/assets/vendor/boxicons/css/boxicons.min.css" rel="stylesheet"/>
+  <link href="src/pages/assets/vendor/glightbox/css/glightbox.min.css" rel="stylesheet"/>
+  <link href="src/pages/assets/vendor/remixicon/remixicon.css" rel="stylesheet"/>
+  <link href="src/pages/assets/vendor/swiper/swiper-bundle.min.css" rel="stylesheet"/>
 
   {/*<!-- Template Main CSS File -->*/}
-  <link href="assets/css/style.css" rel="stylesheet"/>
+  <link href="src/pages/assets/css/style.css" rel="stylesheet"/>
 
       
          <div><header id="header" className="fixed-top d-flex align-items-center">
@@ -56,7 +56,7 @@ export const Navbar = () => {
                 <li><a href="#">Drop Down 4</a></li>
               </ul>
             </li>
-            <li><a className="nav-link scrollto" href="#contact">Contact</a></li>
+            <li><a className="nav-link scrollto" href="/contact">Contact</a></li>
             <li><a className="getstarted scrollto" href="#about">Get Started</a></li>
           </ul>
           <i className="bi bi-list mobile-nav-toggle"></i>
