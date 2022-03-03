07c6eaa26868c6ddba4c4e0e2ffbc3eddd1618b7
jeeljadawala <64966081+jeeljadawala@users.noreply.github.com>
2022-03-03 18:36:07 +0530

    linked contact.js, about.js

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -13,8 +13,9 @@ export const Contact = () => {
           </div>
 
           <div className="col-lg-8" data-aos="fade-up" data-aos-delay="100">
-            <iframe style="border:0; width: 100%; height: 270px;" src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d12097.433213460943!2d-74.0062269!3d40.7101282!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0xb89d1fe6bc499443!2sDowntown+Conference+Center!5e0!3m2!1smk!2sbg!4v1539943755621" frameborder="0" allowfullscreen></iframe>
-            <div className="info mt-4">
+           {/* <iframe style="border:0; width: 100%; height: 270px;" src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d12097.433213460943!2d-74.0062269!3d40.7101282!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0xb89d1fe6bc499443!2sDowntown+Conference+Center!5e0!3m2!1smk!2sbg!4v1539943755621" frameborder="0" allowfullscreen></iframe>
+           */} 
+           <div className="info mt-4">
               <i className="bi bi-geo-alt"></i>
               <h4>Location:</h4>
               <p>A108 Adam Street, New York, NY 535022</p>
--- src/pages/Navbar.js
+++ src/pages/Navbar.js
@@ -1,5 +1,4 @@
 import React from 'react'
-import { Nav } from 'react-bootstrap'
 import { Link } from 'react-router-dom'
 
 
