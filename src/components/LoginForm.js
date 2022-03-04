78db252e3dd4bf475f1459c9a0db076c031f765a
keyap22 <71260297+keyap22@users.noreply.github.com>
2022-03-04 14:28:46 +0530

    Updated login form

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -24,29 +24,50 @@ export const LoginForm = () => {
     }
     return (
         <div className="form-center">
-            <form>
-                <div className="mb-3 mt-3">
-                    <label for="email" className="form-label">Email:</label>
-                    <input type="email" className="form-control" id="email" placeholder="Enter email" name="email" 
-                    required onChange={(e) => { emailHandler(e) }} />
 
+            <form className="login-form" align="center" onSubmit={submit}>
+
+                {/* <div className="form-group row mb-3 mt-3">
+                    <label for="email" className="col-form-label"><strong>Email : </strong></label>
+                    <div className="col-sm-10">
+                        <input type="email" className="form-control" id="email" placeholder="Enter email" name="email"
+                            required onChange={(e) => { emailHandler(e) }} />
+                    </div>
                 </div>
 
-                <div className="mb-3">
-                    <label for="pwd" className="form-label">Password:</label>
-                    <input type="password" className="form-control" id="password" placeholder="Enter password" 
-                    name="password" required onChange={(e) => { passwordHandler(e) }} />
+                <div className="form-group row mb-3">
+                    <label for="pwd" className="col-form-label"><strong> Password : </strong></label>
+                    <div className="col-sm-10">
+                        <input type="password" className="form-control" id="password" placeholder="Enter password"
+                            name="password" required onChange={(e) => { passwordHandler(e) }} />
+                    </div>
+                </div> */}
 
+                <div className="form-group row my-3 mr-2 mb-3">
+                    <label htmlFor="Email" className="col-sm-2 col-form-label"><strong>Email  </strong></label>
+                    <div className="col-sm-10">
+                        <input type="email" id="Email" className="form-control" name="email"
+                            placeholder="Enter Your Email" required onChange={(e) => { emailHandler(e) }} />
+                    </div>
                 </div>
 
-                <div className="form-check mb-3">
+                <div className="form-group row my-3 mr-2 mb-3">
+                    <label htmlFor="Password1" className="col-sm-2 col-form-label"><strong>Password  </strong></label>
+                    <div className="col-sm-10">
+                        <input type="password" id="Password1" className="form-control" name="Password1"
+                            placeholder="Create a strong password" required onChange={(e) => { passwordHandler(e) }} />
+                    </div>
+                </div>
+
+
+                <div className="form-group form-check mb-3">
                     <label className="form-check-label">
                         <input className="form-check-input" type="checkbox" name="remember" /> Remember me
                     </label>
                 </div>
-               
+
                 <input type="submit" className='btn-centre' value="Login" />
-               
+
             </form>
         </div>
     )
