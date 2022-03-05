da654523dd39bd66d1185baf8c73b89af42d05bf
keyap22 <71260297+keyap22@users.noreply.github.com>
2022-03-05 11:16:07 +0530

    Updated About page

--- src/pages/About.js
+++ src/pages/About.js
@@ -7,23 +7,21 @@ export const About = () => {
 
         <div className="row content">
           <div className="col-lg-6" data-aos="fade-right" data-aos-delay="100">
-            <h2>Eum ipsam laborum deleniti velitena</h2>
-            <h3>Voluptatem dignissimos provident quasi corporis voluptates sit assum perenda sruen jonee trave</h3>
+            <h2>eSociety Management</h2>
+            <h3>Web-based software for a society where accounts are maintained correctly and provides transparency within the society members.</h3>
           </div>
           <div className="col-lg-6 pt-4 pt-lg-0" data-aos="fade-left" data-aos-delay="200">
             <p>
-              Ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate
-              velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
-              culpa qui officia deserunt mollit anim id est laborum
+             An interactive system providing all the information regarding gate activities related to them. It will allow the residents to authorize, validate and manage all the visitors from the milkman to the maid and the plumber. Apart from the runtime request, society member can update information prior to the arrival of any parcel, delivery or guest. Periodically report of visitors will be generated which can be accessed by them.
             </p>
             <ul>
-              <li><i className="ri-check-double-line"></i> Ullamco laboris nisi ut aliquip ex ea commodo consequa</li>
-              <li><i className="ri-check-double-line"></i> Duis aute irure dolor in reprehenderit in voluptate velit</li>
-              <li><i className="ri-check-double-line"></i> Ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in</li>
+              <li><i className="ri-check-double-line"></i>Security Staff Attendence</li>
+              <li><i className="ri-check-double-line"></i>Child Security</li>
+              <li><i className="ri-check-double-line"></i>Parking Space Allotment</li>
             </ul>
             <p className="fst-italic">
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore
-              magna aliqua.
+                Inter call facility for runtime requests<br/>
+                Role Based Access
             </p>
           </div>
         </div>
