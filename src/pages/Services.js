adbedc54247d659df8f90ee3860c332bbf947e7a
jeeljadawala <64966081+jeeljadawala@users.noreply.github.com>
2022-03-08 20:01:40 +0530

    changed icons in services

--- src/pages/Services.js
+++ src/pages/Services.js
@@ -17,7 +17,7 @@ export const Services = () => {
                         <div className="row">
                             <div className="col-md-6 d-flex align-items-stretch">
                                 <div className="icon-box aos-init aos-animate" data-aos="zoom-in" data-aos-delay="100">
-                                    <div className="icon"><i className="bx bxl-dribbble"></i></div>
+                                    <div className="icon"><i className="bi bi-shield-fill-check"></i></div>
                                     <h4><a href="">Gate/Security Management</a></h4>
                                     <p>Handling Unexpected Guests by spotting entry at gate triggering notification/IVR calls for approval.</p>
                                     <p>Prescheduling the time of expected guests ,parcel.</p>
@@ -27,7 +27,7 @@ export const Services = () => {
 
                             <div className="col-md-6 d-flex align-items-stretch mt-4 mt-lg-0">
                                 <div className="icon-box aos-init aos-animate" data-aos="zoom-in" data-aos-delay="200">
-                                    <div className="icon"><i className="bx bx-file"></i></div>
+                                    <div className="icon"><i className="bx bx-car"></i></div>
                                     <h4><a href="">Vehicle Management</a></h4>
                                     <p>Allocate approved number of parking slots to each flat/villa.</p>
                                 </div>
@@ -35,7 +35,7 @@ export const Services = () => {
 
                             <div className="col-md-6 d-flex align-items-stretch mt-4">
                                 <div className="icon-box aos-init aos-animate" data-aos="zoom-in" data-aos-delay="300">
-                                    <div className="icon"><i className="bx bx-tachometer"></i></div>
+                                    <div className="icon"><i className="bi bi-person-bounding-box"></i></div>
                                     <h4><a href="">Child Safety Alert</a></h4>
                                     <p>To monitor/control children exiting the campus (goes to residents).</p>
                                     <p>To schedule restrict/permit children from exiting the society.</p>
@@ -44,7 +44,7 @@ export const Services = () => {
 
                             <div className="col-md-6 d-flex align-items-stretch mt-4">
                                 <div className="icon-box aos-init aos-animate" data-aos="zoom-in" data-aos-delay="400">
-                                    <div className="icon"><i className="bx bx-world"></i></div>
+                                    <div className="icon"><i className="bi bi-check-square"></i></div>
                                     <h4><a href="">Security Staff Attendence</a></h4>
                                     <p>Detailed reports with flexibility to define multiple configurable shifts for different type of staff.</p>
                                 </div>
