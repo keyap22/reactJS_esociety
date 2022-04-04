d835637d0a922436bbe0e122042d8b3f26f24b6a
jeeljadawala <64966081+jeeljadawala@users.noreply.github.com>
2022-04-04 23:55:02 +0530

    minor update

--- src/App.js
+++ src/App.js
@@ -180,6 +180,7 @@ const verifyOTP=(e)=>
         <Route path="/about" element={<About />}></Route>
         {/* <Route path="/home" element={<Home />}></Route> */}
         <Route path="/login" element={<LoginForm />}></Route>
+      
         <Route path="/signup" element={<SignupForm />}></Route>
         <Route path="/forgotpassword" element={<ForgotPassword />}></Route>
         <Route path="/services" element={<Services />}></Route>
